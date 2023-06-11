d4e6f5c0099eb282c97fd6be785f127d629380ab
秋叶红 <793983383@qq.com>
2023-06-11 13:32:35 +0800

    ff

---
+++ iOS/OpenGLVideoPlayer/OpenGLVideoPlayer/Lib/EncodeAudio.h
@@ -0,0 +1,16 @@
+//
+//  EncodeAudio.h
+//  OpenGLVideoPlayer
+//
+//  Created by YeQiu on 2023/6/11.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EncodeAudio : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS/OpenGLVideoPlayer/OpenGLVideoPlayer/Lib/EncodeAudio.m
@@ -0,0 +1,12 @@
+//
+//  EncodeAudio.m
+//  OpenGLVideoPlayer
+//
+//  Created by YeQiu on 2023/6/11.
+//
+
+#import "EncodeAudio.h"
+
+@implementation EncodeAudio
+
+@end
